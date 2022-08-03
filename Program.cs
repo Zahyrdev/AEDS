47087c1a908edc32cbc067dc15aa61eaab2976c6
zahyr <102634224+Zahyrdev@users.noreply.github.com>
2022-08-03 20:34:32 -0300

    .

--- Program.cs
+++ Program.cs
@@ -3,13 +3,37 @@
 class Program
 {
     static void Main(string[] args){
+        
+    }
+
+    public void PrimeiraQuestao(){
         double R, V;
         Console.WriteLine("Entre com o parâmetro: ");
         R = double.Parse(Console.ReadLine());
         Console.WriteLine(Lefuncao(R));
 
-        int Lefuncao(double V){
+        double Lefuncao(double V){
             return (4 * 3.14 * Math.Pow(V, 3)) / 3;
         }
     }
-}
+
+    public void SegundaQuestao(){
+        double R, V;
+        Console.WriteLine("Entre com o parâmetro: ");
+        R = double.Parse(Console.ReadLine());
+        Console.WriteLine(Lefuncao(R));
+
+        double Lefuncao(double V)
+        {
+            V = Math.Floor(V);
+            Console.WriteLine(V);
+            return (4 * 3.14 * Math.Pow(V, 3)) / 3;
+        }
+    }
+
+        public void TerceiraQuestao(){
+        float [] A = new float [10];
+        LeVetor(A);
+        
+        }
+    }
