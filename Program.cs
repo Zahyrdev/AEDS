eb6585d1ba2331bdd3340ead224380e3ea4ccafd
zahyr <102634224+Zahyrdev@users.noreply.github.com>
2022-08-03 20:05:08 -0300

    .

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args){
+        double R, V;
+        Console.WriteLine("Entre com o parâmetro: ");
+        R = double.Parse(Console.ReadLine());
+        Console.WriteLine(Lefuncao(R));
+
+        int Lefuncao(double V){
+            return (4 * 3.14 * Math.Pow(V, 3)) / 3;
+        }
+    }
+}
