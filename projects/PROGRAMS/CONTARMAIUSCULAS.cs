9b86ed500cc004afa2970759d0808fc720f08174
zahyr <102634224+Zahyrdev@users.noreply.github.com>
2022-08-24 20:34:23 -0300

    V

---
+++ projects/PROGRAMS/CONTARMAIUSCULAS.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main(string[] args){
+        string texto = "";
+            do
+            {
+                texto = Console.ReadLine();
+                if (texto.ToUpper() != "FIM")
+                    Console.WriteLine($"Foram encontradas {ContaMaiusculas(texto)} letras maiúsculas no texto");
+            } while(texto.ToUpper() != "FIM");
+        }
+
+        static int ContaMaiusculas(string texto)
+        {
+            int cont = 0;
+            foreach (char c in texto)
+            {
+                if (Char.IsUpper(c))
+                    cont++;
+            }
+            return cont;
+        }
+    }
